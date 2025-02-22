36cdba949a640cef2970f0ad22b0bc86c70d0aa8
taladib123 <taladib1234@gmail.com>
2025-02-22 11:51:41 +0200

    add icon and splash icon

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,13 @@
-import { Stack } from "expo-router";
+import { useRouter, Stack, Slot } from "expo-router";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, Dimensions, View } from "react-native";
+
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(main)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
   );
 }
